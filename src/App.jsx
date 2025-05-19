d658bb80dc5065d3db97f5c31e325e10adb3b45b
Byron Hambly <bhambly@blockstream.com>
2025-05-19 14:27:06 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import "./App.css";
 import blockdata from "./assets/data.json";
 
+const url = "https://blockstream.info/liquid"
+
 function Block({ block }) {
   let extra = {
     false: { cls: "inactive", icon: "." },
     true: { cls: "active", icon: "✓" },
   };
-  const url = `https://blockstream.info/liquid/block/${block.hash}`
   const title = `Block height: ${block.height} - version 0x${block.versionhex}`
   const active = block.simplicity
   return (
-    <a href={url} target="_blank" title={title} className={"block " + extra[active].cls}>{extra[active].icon}</a>
+    <a href={`${url}/block/${block.hash}`} target="_blank" title={title} className={"block " + extra[active].cls}>{extra[active].icon}</a>
   );
 }
 
@@ -20,14 +21,25 @@ function Blocks({ blocks }) {
 
 function App() {
   const blocks = Object.values(blockdata.blocks).reverse().slice(0, 1500)
+  console.log(blockdata)
+  const {height, hash, deployments} = blockdata.deployments
+  const {simplicity} = deployments
+  console.log(simplicity)
+  const {bip9} = simplicity
+  const stats = bip9.statistics
 
   return (
     <div>
       <h1>SIMPLICITY Signalling Tracker</h1>
-      <p>Current Liquid tip: </p>
-      <p>Simplicity BIP-9 deployment status: </p>
-      <p>Signalling period: 10080 blocks (1 week)</p>
-      <p>Signalling threshold: 100% required to lock-in</p>
+      <p>Liquid chain height: <a href={`${url}/block/${hash}`} target="_blank">{height}</a></p>
+      <p>Simplicity BIP-9 deployment: </p>
+      <p>Active: {simplicity.active.toString()}</p>
+      <p>Status: {simplicity.bip9.status}</p>
+      <p>Period: {stats.period}</p>
+      <p>Elapsed: {stats.elapsed}</p>
+      <p>Count: {stats.count}</p>
+      <p>Threshold: {stats.threshold}</p>
+      <p>Possible in this period: {stats.possible.toString()}</p>
       <p>Last {blocks.length} blocks</p>
       <Blocks blocks={blocks}></Blocks>
     </div>
